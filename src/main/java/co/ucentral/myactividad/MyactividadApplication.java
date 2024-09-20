4ba2fc78215e7eec5461d382fd637d6766ac3923
Juan <juan12@gmail.com>
2024-09-19 19:24:08 -0500

    (nueva): configuracion de mi actividad con springt boot

---
+++ src/main/java/co/ucentral/myactividad/MyactividadApplication.java
@@ -0,0 +1,13 @@
+package co.ucentral.myactividad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyactividadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyactividadApplication.class, args);
+	}
+
+}
